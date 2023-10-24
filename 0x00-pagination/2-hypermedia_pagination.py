f4c05a4187961a31449be6f74ffc2f499f31b51d
kevinnevs <kevinnjuguna2@gmail.com>
2023-10-24 09:55:43 +0300

    Fixing indentation, adding comments on function init

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -40,7 +40,7 @@ class Server:
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page,page_size)
+        start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
             return []
