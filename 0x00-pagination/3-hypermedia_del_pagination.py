6efa651d9aa9ec11b5913de60a80f7a9ba26c64a
kevinnevs <kevinnjuguna2@gmail.com>
2023-10-24 09:58:29 +0300

    Adding comments, fixing additional spacing

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -2,9 +2,7 @@
 """
 Deletion-resilient hypermedia pagination
 """
-
 import csv
-import math
 from typing import Dict, List 
 
 
@@ -14,6 +12,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
         self.__indexed_dataset = None
 
